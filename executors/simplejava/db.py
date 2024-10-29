e08d593b67da2b8f44bf700894d22164e10e1c47
Evan Danaher <github@edanaher.net>
2024-10-28 21:52:15 -0400

    Add db.py.  Whoops.

---
+++ executors/simplejava/db.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, BLOB, ForeignKey, UniqueConstraint
+
+engine = create_engine("sqlite+pysqlite:////tmp/idecl.db")
+metadata_obj = MetaData()
+
+user_table = Table(
+    "users",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, unique=True, nullable=False),
+    Column("name", String)
+)
+
+projects_table = Table(
+    "projects",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("owner", Integer, ForeignKey("users.id"), nullable=False),
+    Column("name", String),
+    UniqueConstraint("owner", "name", name="uniq_project_owner_name")
+)
+
+files_table = Table(
+    "files",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("project_id", Integer, ForeignKey("projects.id"), nullable=False),
+    Column("name", String),
+    Column("contents", BLOB),
+    UniqueConstraint("project_id", "name", name="uniq_file_project_name")
+)
+
+metadata_obj.create_all(engine)

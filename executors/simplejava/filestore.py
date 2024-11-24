68e7161302873dee3ce003eee1059a7ba7cc7539
Evan Danaher <github@edanaher.net>
2024-11-24 13:39:17 -0500

    Fix initializing db users from env.

--- executors/simplejava/filestore.py
+++ executors/simplejava/filestore.py
@@ -10,10 +10,9 @@ from db import engine
 
 
 with engine.connect() as conn:
-    count = conn.execute(text("SELECT COUNT(*) FROM users")).first()
+    count = conn.execute(text("SELECT COUNT(*) FROM users")).first()[0]
     if count == 0:
         conn.execute(text("INSERT INTO users (email) VALUES (:email)"), [{"email": e} for e in os.environ.get("USERS").split(",")])
-        conn.execute(text("INSERT INTO projects (owner, name) VALUES (:user_id, 'default')"), [{"user_id": n + 1} for n in range(0, len(os.environ.get("USERS").split(",")))])
         conn.commit()
 
 

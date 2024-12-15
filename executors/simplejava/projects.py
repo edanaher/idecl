3e4240c02363f7ef0b7d9772f61395b8ffadc9fe
Evan Danaher <github@edanaher.net>
2024-12-15 15:57:49 -0500

    In UI, don't allow recloning projects.
    It was already blocked on permissions, but now there's no button.

--- executors/simplejava/projects.py
+++ executors/simplejava/projects.py
@@ -30,13 +30,16 @@ def projects(classroom):
     with engine.connect() as conn:
         projects = conn.execute(text("""
             SELECT id, name, GROUP_CONCAT(cancloneassignment) AS cancloneassignment FROM (
-                SELECT projects.id, projects.name, GROUP_CONCAT(rp_cloneassignment.id) AS cancloneassignment FROM projects
+                SELECT projects.id, projects.name,
+                       GROUP_CONCAT(rp_cloneassignment.id) AND (already_cloned.id IS NULL) AS cancloneassignment
+                FROM projects
                 LEFT JOIN users_roles ON ((projects.id = users_roles.project_id OR users_roles.project_id IS NULL)
                                           AND (users_roles.classroom_id IS NULL or users_roles.classroom_id=:classroom))
                 LEFT JOIN roles_permissions ON (users_roles.role_id = roles_permissions.role_id)
                 LEFT JOIN projects_tags ON (roles_permissions.tag_id = projects_tags.tag_id AND projects_tags.project_id=projects.id)
                 LEFT JOIN classrooms_tags ON (roles_permissions.tag_id = projects_tags.tag_id AND classrooms_tags.classroom_id=:classroom)
                 LEFT JOIN roles_permissions AS rp_cloneassignment ON (users_roles.role_id = rp_cloneassignment.role_id AND rp_cloneassignment.permission_id=:perm_cloneassignment)
+                LEFT JOIN projects AS already_cloned ON (already_cloned.parent_id=projects.id AND already_cloned.owner=:user AND already_cloned.cloned_as_assignment=TRUE)
                 WHERE projects.classroom_id=:classroom
                 AND users_roles.user_id=:user
                 AND roles_permissions.permission_id=:perm

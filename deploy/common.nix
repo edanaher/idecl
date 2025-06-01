71abecf3836bdc090611bf99b898b6f687104404
Evan Danaher <github@edanaher.net>
2025-06-01 16:00:22 -0400

    Use non-tmp path for nginx upload directory.
    This should fix large file saving.

--- deploy/common.nix
+++ deploy/common.nix
@@ -221,6 +221,7 @@ rec {
           proxy_set_header Host $host${if dev then ":5000" else ""};
           proxy_set_header X-Forwarded-For $remote_addr;
           proxy_buffering off;
+          client_body_temp_path /var/cache/nginx/client_body;
         '';
       };
       enableACME = usessl;
